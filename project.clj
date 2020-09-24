ca584f158b735271d8b607b316aa78ef2d2aa115
Donald Clark Jackson <dcj@clark-communications.com>
2020-09-24 08:20:40 -0700

    update deps, clojure, etc

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
-(defproject com.damballa/inet.data "0.5.8-SNAPSHOT"
+(defproject com.dcj/inet.data "0.5.9-SNAPSHOT"
   :description "Represent and manipulate various Internet entities as data."
   :url "http://github.com/llasram/inet.data"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :global-vars {*warn-on-reflection* true}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [hier-set "1.1.2"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.dcj/hier-set "1.2.1-SNAPSHOT"]]
   :plugins [[lein-ragel "0.1.0"]
-            [codox "0.6.6"]]
+            [codox "0.10.7"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java" "target/ragel"]
   :ragel-source-paths ["src/ragel"]
-  :javac-options ["-target" "1.7" "-source" "1.7"]
+  :javac-options ["-target" "1.10" "-source" "1.10"]
   :prep-tasks ["ragel" "javac"]
   :codox {:src-dir-uri "https://github.com/damballa/inet.data/blob/master/"
           :src-linenum-anchor-prefix "L"
@@ -20,11 +20,15 @@
           :output-dir "tmp/codox"}
   :aliases {"prep" ["do" "ragel," "javac"]
             "all" ["with-profile" ~(str "+clojure-1-6:"
-                                        "+clojure-1-7")]}
+                                        "+clojure-1-7"
+                                        "+clojure-1-10")]}
   :profiles {:provided {:dependencies
                         [[byteable "0.2.0"]
-                         [com.damballa/abracad "0.4.12"]]}
+                         [com.damballa/abracad "0.4.13"]]}
              :clojure-1-6 {:dependencies
                            [[org.clojure/clojure "1.6.0"]]}
              :clojure-1-7 {:dependencies
-                           [[org.clojure/clojure "1.7.0"]]}})
+                           [[org.clojure/clojure "1.7.0"]]}
+             :clojure-1-10 {:dependencies
+                           [[org.clojure/clojure "1.10.1"]]}
+             })
